ccacddc502f07d3bdd1a939dd3b5822aca8805ae
jackyang <ydzhony1@163.com>
2021-12-24 20:56:56 +0800

    init project react
    init project react

---
+++ example/src/app.js
@@ -0,0 +1,7 @@
+/*** examples/src/app.js ***/
+import React from 'react'
+import { render } from 'react-dom'
+import ReactDemo from '../../src'
+
+const App = () => <ReactDemo />
+render(<App />, document.getElementById('root'))
---
+++ src/index.js
@@ -0,0 +1,6 @@
+/*** src/index.js ***/
+import React from 'react';
+const ReactDemo = () => (
+ <h1>hello world, This is my first component.</h1>
+);
+export default ReactDemo;
